121515861a0ae445fba5a9e4125b381bb05cfe86
cawauchi6204 <cawauchi6204@gmail.com>
2020-11-27 23:24:54 +0900

    index.tsxでappコンポーネントをThemeProviderでラップ

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { theme } from './theme/theme'
+import { ThemeProvider } from '@material-ui/styles';
 
 import './index.css';
 import App from './App';
@@ -7,7 +9,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
