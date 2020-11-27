27d058df794df0a57ad272c795cc8f4d693d17eb
cawauchi6204 <cawauchi6204@gmail.com>
2020-11-27 22:59:30 +0900

    Error pape の実装完了

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Typography, Button } from '@material-ui/core'
+import { Link } from 'react-router-dom'
+
+const ErrorPage = () => (
+  <>
+    <img src={`${process.env.PUBLIC_URL}/404error.png`} style={{ width: '50%', height: 'auto' }} />
+    <Typography variant="h1">ページが見つかりません</Typography>
+    <Link to="/" style={{ textDecoration: 'none' }}>
+      <Button variant="contained" color="primary">トップページに戻る</Button>
+    </Link>
+  </>
+)
+
+export default ErrorPage
